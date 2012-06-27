ea88d4e630de5cfd3eaf773acee26c2c0dfd0708
Kevin Fullerton <github@kenwa-solutions.co.uk>
2012-06-27 17:18:19 +0100

    Started on Quote plugin

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source :rubygems
 
 gem 'cinch'
 gem 'nokogiri'
+gem 'mongoid', '~> 3.0.0.rc'
--- bot.rb
+++ bot.rb
@@ -5,6 +5,38 @@ require 'cgi'
 
 Bundler.require()
 
+class Quote
+	include Mongoid::Document
+end
+
+class QuotePlugin
+	include Cinch::Plugin
+
+	match /addquote (.+)/, :method => :addquote
+	match /quote$/, :method => :randomquote
+	match /quote (.+)/, :method => :searchquote
+	listen_to :join, :method => :joined
+
+	def joined(m)
+		#m.reply "#{m.user} joined", false unless m.user.nick == bot.nick
+	end
+
+	def randomquote(m)
+		quote = Quote.skip(rand(Quote.count)).limit(1).first
+		m.reply(quote.quote || "No quotes added yet - go on, be the first", false)
+	end
+
+	def addquote(m, quote)
+		new_quote = Quote.create(quote: quote)
+		m.reply "Added quote with ID #{new_quote.id}", false
+	end
+
+	def searchquote(m, search)
+		#m.reply "Will search for '#{search}'", false
+	end
+end
+
+
 class UrbanDictionary
 	include Cinch::Plugin
 
@@ -24,9 +56,10 @@ bot = Cinch::Bot.new do
 		c.nick = "otquote"
 		c.realname = "OT Quotes"
 		c.user = "otquote"
-		c.channels = ["#surrey-ot"]
-		c.plugins.plugins = [UrbanDictionary]
+		c.channels = ["#ot-quote"]
+		c.plugins.plugins = [UrbanDictionary,QuotePlugin]
 		c.verbose = true
 	end
 end
+Mongoid.load!("mongoid.yml","production")
 bot.start
